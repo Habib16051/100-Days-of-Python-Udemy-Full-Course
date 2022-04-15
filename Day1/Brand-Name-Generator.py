23aa281fd3f5b306318a7ea165b195564233a1b5
Habib16051 <habibmhr143@gmail.com>
2022-04-15 20:42:49 +0600

    day 2 added

---
+++ Day1/Brand-Name-Generator.py
@@ -0,0 +1,14 @@
+# Create a greetings for your program
+
+print('Welcome to the brand name generator!!')
+
+# Ask the user for the city they grow up
+
+city = input('Which city you were grow up?\n')
+# Ask the user for their pet name
+pet = input('What is your pet name?\n')
+
+# Combine their city and pet name
+
+combined = city + ' ' + pet
+print("Your Brand name could be :", combined)
--- Intro.py
+++ Intro.py
@@ -1,1 +1,7 @@
-print('Whats up everyone!')
+# # print('Whats up everyone!')
+#
+# x = input('Whats your name?')
+#
+# print(x)
+
+print(len(input('Tell me your name, kinldy!')))
